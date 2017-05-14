6d0b5d93e453bd1b4b1ac37eae6a200cc5d65553
liuqixing <liuqixing@gmail.com>
2017-05-14 17:02:25 +0800

    rename xslib.js to delib.js

--- build-min.sh
+++ build-min.sh
@@ -4,6 +4,6 @@ java -jar "tools/yuicompressor-2.4.8.jar" "dist/js/jquery-edition/load-toast.js"
 java -jar "tools/yuicompressor-2.4.8.jar" "dist/js/jquery-edition/msg-confirm.js" -o "dist/js/jquery-edition/msg-confirm.min.js" --charset utf-8
 java -jar "tools/yuicompressor-2.4.8.jar" "dist/js/jquery-edition/msg-page.js" -o "dist/js/jquery-edition/msg-page.min.js" --charset utf-8
 java -jar "tools/yuicompressor-2.4.8.jar" "dist/js/jquery-edition/top-toast.js" -o "dist/js/jquery-edition/top-toast.min.js" --charset utf-8
-java -jar "tools/yuicompressor-2.4.8.jar" "dist/js/jquery-edition/xslib.js" -o "dist/js/jquery-edition/xslib.min.js" --charset utf-8
+java -jar "tools/yuicompressor-2.4.8.jar" "dist/js/jquery-edition/delib.js" -o "dist/js/jquery-edition/delib.min.js" --charset utf-8
 
 java -jar "tools/yuicompressor-2.4.8.jar" "dist/style/main.css" -o "dist/style/main.min.css" --charset utf-8
