9c9d04ac323379b583599c6da002d31345f54ae6
liuqixing <liuqixing@dekuan.org>
2017-07-09 17:30:01 +0800

    1, rewrite load-toast.js and rename its name to loading.js

--- build-min.sh
+++ build-min.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-java -jar "tools/yuicompressor-2.4.8.jar" "dist/js/jquery-edition/load-toast.js" -o "dist/js/jquery-edition/load-toast.min.js" --charset utf-8
+java -jar "tools/yuicompressor-2.4.8.jar" "dist/js/jquery-edition/loading.js" -o "dist/js/jquery-edition/loading.min.js" --charset utf-8
 java -jar "tools/yuicompressor-2.4.8.jar" "dist/js/jquery-edition/msg-confirm.js" -o "dist/js/jquery-edition/msg-confirm.min.js" --charset utf-8
 java -jar "tools/yuicompressor-2.4.8.jar" "dist/js/jquery-edition/msg-page.js" -o "dist/js/jquery-edition/msg-page.min.js" --charset utf-8
 java -jar "tools/yuicompressor-2.4.8.jar" "dist/js/jquery-edition/top-toast.js" -o "dist/js/jquery-edition/top-toast.min.js" --charset utf-8
